23167092af5d1c8c619b5462dc8e125538991328
Fued Kraide <omekra@hotmail.com>
2025-12-29 11:55:31 +1000

    fix(api): add onDelete behavior to user relations in schema

--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -16,7 +16,7 @@ export async function resetPassword(app: FastifyInstance) {
         summary: 'Reset user password',
         body: z.object({
           code: z.string(),
-          password: z.string().min(6)
+          password: z.string().min(6),
         }),
         response: {
           204: z.null(),
@@ -38,14 +38,21 @@ export async function resetPassword(app: FastifyInstance) {
 
       const passwordHash = await hash(password, 6)
 
-      await prisma.user.update({
-        where: {
-          id: tokenFromCode.userId,
-        },
-        data: {
-          passwordHash,
-        },
-      })
+      await prisma.$transaction([
+        prisma.user.update({
+          where: {
+            id: tokenFromCode.userId,
+          },
+          data: {
+            passwordHash,
+          },
+        }),
+        prisma.token.delete({
+          where: {
+            id: code,
+          },
+        }),
+      ])
 
       return reply.status(204).send()
     },
