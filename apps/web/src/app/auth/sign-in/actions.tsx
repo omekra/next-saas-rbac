3923be80e0a887279d56f056f160fd5c907e6886
Fued Kraide <omekra@hotmail.com>
2025-12-31 09:42:24 +1000

    feat(auth): refactor sign-in functionality and create SignInForm component

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -2,12 +2,21 @@
 
 import { signInWithPassword } from '@/http/sign-in-with-password'
 
-export async function signInWithEmailAndPassword(data: FormData) {
+export async function signInWithEmailAndPassword(
+  previousState: unknown,
+  data: FormData,
+) {
+  console.log('🚀 ~ previousState:', previousState)
+
   const { email, password } = Object.fromEntries(data)
+
+  await new Promise((resolve) => setTimeout(resolve, 2000))
 
   const result = await signInWithPassword({
     email: String(email),
     password: String(password),
   })
   console.log('🚀 ~ result:', result)
+
+  return 'Success!'
 }
