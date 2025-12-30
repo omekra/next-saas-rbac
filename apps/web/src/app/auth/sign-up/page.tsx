e27bb9a6a294e0796edff045fbc61e82bd69a15b
Fued Kraide <omekra@hotmail.com>
2025-12-30 18:31:46 +1000

    feat(auth): implement ForgotPasswordPage and SignUpPage with GitHub sign-up option

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -7,32 +7,39 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="name">Name</Label>
+        <Input name="name" id="name" />
+      </div>
+
       <div className="space-y-1">
         <Label htmlFor="email">Email</Label>
         <Input name="email" type="email" id="email" />
       </div>
 
       <div className="space-y-1">
+        <Label htmlFor="password_confirmation">Confirm your password</Label>
+        <Input
+          name="password_confirmation"
+          type="password"
+          id="password_confirmation"
+        />
+      </div>
+
+      <div className="space-y-1">
         <Label htmlFor="password">Password</Label>
         <Input name="password" type="password" id="password" />
-
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          Forgot your password?
-        </Link>
       </div>
 
       <Button type="submit" className="w-full">
-        Sign in with email
+        Create account
       </Button>
 
       <Button variant="link" className="w-full" size="sm" asChild>
-        <Link href="/auth/sign-up">Create new account</Link>
+        <Link href="/auth/sign-in">Already registered? Sign in</Link>
       </Button>
 
       <Separator />
@@ -43,7 +50,7 @@ export default function SignInPage() {
           alt="Github icon"
           className="mr-2 size-4 dark:invert"
         />
-        Sign in with Github
+        Sign up with Github
       </Button>
     </form>
   )
