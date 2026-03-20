36e0c980b2b3ee5c6b8886ba4df12375b3b83057
Fued Kraide <omekra@hotmail.com>
2026-03-20 15:52:27 +1000

    feat: using environment variables

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,7 +1,7 @@
 import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
-import rocketseatIcon from '@/assets/rocketseat-icon.svg'
+import fireIcon from '@/assets/fire-icon.svg'
 import { ability } from '@/auth/auth'
 
 import { OrganizationSwitcher } from './organization-switcher'
@@ -15,11 +15,7 @@ export async function Header() {
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
-        <Image
-          src={rocketseatIcon}
-          alt="Rocketseat"
-          className="light:invert size-6"
-        />
+        <Image src={fireIcon} alt="Fire" className="light:invert size-6" />
 
         <Slash className="size-3 -rotate-[24deg] text-border" />
 
--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -2,7 +2,7 @@ import { getCookie } from 'cookies-next'
 import ky from 'ky'
 
 export const api = ky.create({
-  prefixUrl: 'http://localhost:3333',
+  prefixUrl: process.env.NEXT_PUBLIC_API_URL,
   hooks: {
     beforeRequest: [
       async (request) => {
